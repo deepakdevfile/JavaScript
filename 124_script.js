6641b743278fdc84b095a3e8e76b468ff3e9f8ec
deepakdevfile <deepakdevfile@gmail.com>
2026-07-16 07:08:11 +0530

    CSS Grid concepts

---
+++ 124_script.js
@@ -0,0 +1,18 @@
+let menu_items_position = document.getElementById("menu-items-position");
+let menu_items = document.getElementById("menu-items");
+let show = document.getElementById("show");
+
+let main_nav_list = document.getElementById("main-nav-list");
+let main_nav = document.getElementById("main-nav");
+
+menu_items_position.addEventListener("change", function(e){
+    main_nav_list.css("justify-content", e.target.find("option:selected").val());
+});
+
+menu_items.addEventListener("change", function(e){
+    $("#main-nav-list li").css("flex", $(this).find("option:selected").val());
+});
+
+show.addEventListener("change", function(e){
+    main_nav.removeClass("outlines").addClass($(this).find("option:selected").val());
+});
